8dc7e3cb546e19b0f990f9ac1538e45f2cd4d028
Sinesio Junior <sjunior.ti@gmail.com>
2017-05-12 10:24:54 +0200

    Add readme and setup script to install all GEMs used

---
+++ README.md
@@ -0,0 +1,5 @@
+to install node + npm : `brew install node`
+run `sh setup.sh`
+
+
+gulp serve:dev
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+npm install
+sudo gem install thread_safe -v 0.3.5
+sudo gem install tzinfo -v 1.2.2
+sudo gem install coffee-script-source -v 1.10.0
+sudo gem install colorator -v 0.1
+sudo gem install ffi -v 1.9.10
+sudo gem install ethon -v 0.9.0
+sudo gem install gemoji -v 2.1.0
+sudo gem install net-dns -v 0.8.0
+sudo gem install sawyer -v 0.7.0
+sudo gem install octokit -v 4.3.0
+sudo gem install public_suffix -v 1.5.3
+sudo gem install typhoeus -v 0.8.0  
+sudo gem install github-pages-health-check -v 1.1.0
+sudo gem install sass -v 3.4.22
+sudo gem install jekyll-sass-converter -v 1.3.0
+sudo gem install rb-fsevent -v 0.9.7
+sudo gem install listen -v 3.0.6
+sudo gem install jekyll-watch -v 1.4.0
+sudo gem install kramdown -v 1.10.0
+sudo gem install rouge -v 1.10.1
+sudo gem install jekyll -v 3.1.5
+sudo gem install jekyll-coffeescript -v 1.0.1
+sudo gem install jekyll-feed -v 0.5.1
+sudo gem install jekyll-gist -v 1.4.0
+sudo gem install jekyll-github-metadata -v 1.11.1
+sudo gem install html-pipeline -v 2.4.1
+sudo gem install jekyll-mentions -v 1.1.2
+sudo gem install jekyll-paginate -v 1.1.0
+sudo gem install jekyll-redirect-from -v 0.10.0
+sudo gem install jekyll-seo-tag -v 1.4.0
+sudo gem install jekyll-sitemap -v 0.10.0
+sudo gem install jemoji -v 0.6.2
+sudo gem install terminal-table -v 1.5.2
+sudo gem install github-pages -v 79
